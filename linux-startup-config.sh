42e132db7325e74c682973a7d95d9d49ad7340e7
Mateus-Mota <mateusmota0100@gmail.com>
2022-07-25 22:19:03 -0300

    Update linux-startup-config.sh

--- linux-startup-config.sh
+++ linux-startup-config.sh
@@ -13,16 +13,17 @@ groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
-echo "Criando usuários..."
-
+echo "Criando usuários do grupo de administração..."
 useradd carlos -m -s /bin/bash -p ${openssl passwd -crypt Senha123} -G GRP_ADM
 useradd maria -m -s /bin/bash -p ${openssl passwd -crypt Senha123} -G GRP_ADM
 useradd joao -m -s /bin/bash -p ${openssl passwd -crypt Senha123} -G GRP_ADM
 
+echo "Criando usuários do grupo de vendas..."
 useradd debora -m -s /bin/bash -p ${openssl passwd -crypt Senha123} -G GRP_VEN
 useradd sebastiao -m -s /bin/bash -p ${openssl passwd -crypt Senha123} -G GRP_VEN
 useradd roberto -m -s /bin/bash -p ${openssl passwd -crypt Senha123} -G GRP_VEN
 
+echo "Criando usuários do grupo de segurança..."
 useradd josefina -m -s /bin/bash -p ${openssl passwd -crypt Senha123} -G GRP_SEC
 useradd amanda -m -s /bin/bash -p ${openssl passwd -crypt Senha123} -G GRP_SEC
 useradd rogerio -m -s /bin/bash -p ${openssl passwd -crypt Senha123} -G GRP_SEC
